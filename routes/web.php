817f7cec97a004b6ef1dd82c67c836b6586c8873
Ihsanurdin <isan7964@gmail.com>
2024-06-20 10:55:02 +0700

    sinvent

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/', [\App\Http\Controllers\DashboardController::class,'index'])->name('dashboard');
+Route::resource('/kategori', \App\Http\Controllers\KategoriController::class)->middleware('auth');
+Route::resource('/barang', \App\Http\Controllers\BarangController::class)->middleware('auth');
+Route::resource('/barangmasuk', \App\Http\Controllers\BarangMasukController::class)->middleware('auth');
+Route::resource('/barangkeluar', \App\Http\Controllers\BarangKeluarController::class)->middleware('auth');
+
+Route::get('register', [\App\Http\Controllers\RegistrasiController::class,'create'])->name('register')->middleware('guest');
+Route::post('register', [\App\Http\Controllers\RegistrasiController::class,'store']);
+ 
+Route::get('login', [\App\Http\Controllers\LoginController::class,'index'])->name('login')->middleware('guest');
+Route::post('login', [\App\Http\Controllers\LoginController::class,'authenticate']);
+
+Route::post('logout', [\App\Http\Controllers\LoginController::class,'logout']);
+Route::get('logout', [\App\Http\Controllers\LoginController::class,'logout']);
